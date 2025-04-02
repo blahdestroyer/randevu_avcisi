99199a13cd24ffeed4a6e342f5240d7d3822f7e6
blahdestroyer <marvin.ordinarus@gmail.com>
2025-04-02 10:45:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from scheduler import zamanlayici_baslat
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Randevu Avcısı Aktif"
 
 if __name__ == "__main__":
+    from scheduler import zamanlayici_baslat
     zamanlayici_baslat()
+    app.run(host="0.0.0.0", port=10000)  # sahte port
