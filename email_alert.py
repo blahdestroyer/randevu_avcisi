915d017b0ff8389086fa852965960f63631f53d3
blahdestroyer <marvin.ordinarus@gmail.com>
2025-04-02 10:29:07 +0300

    Update email_alert.py

--- email_alert.py
+++ email_alert.py
@@ -1,9 +1,10 @@
 import smtplib
+import os
 from email.mime.text import MIMEText
 
 def send_email(konu, icerik):
-    gmail_user = "marvin.ordinarus@gmail.com"
-    gmail_password = "fway lxxi vuhj syal"  # Uygulama şifresi
+    gmail_user = os.getenv("EMAIL_USER")
+    gmail_password = os.getenv("EMAIL_PASS")
 
     msg = MIMEText(icerik)
     msg["Subject"] = konu
@@ -11,7 +12,7 @@ def send_email(konu, icerik):
     msg["To"] = gmail_user
 
     try:
-        with smtpllib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
             server.login(gmail_user, gmail_password)
             server.sendmail(gmail_user, [gmail_user], msg.as_string())
             print("[MAIL] E-posta gönderildi.")
