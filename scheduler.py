3736229b63e37492f25d035a2a560cf0c1868cdf
blahdestroyer <marvin.ordinarus@gmail.com>
2025-04-02 10:52:24 +0300

    Delete scheduler.py

--- scheduler.py
+++
@@ -1,9 +0,0 @@
-from kontrol import randevu_kontrol
-import time
-
-def zamanlayici_baslat():
-    while True:
-        print("[ZAMANLAYICI] Randevu kontrolü başlatıldı.")
-        randevu_kontrol()
-        print("[ZAMANLAYICI] 5 dakika bekleniyor...")
-        time.sleep(300)
