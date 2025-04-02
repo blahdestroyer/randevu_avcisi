b7c4ce19f77378080481f87641963594055c03f5
blahdestroyer <marvin.ordinarus@gmail.com>
2025-04-02 16:04:34 +0300

    Add files via upload

---
+++ email_alert.py
@@ -0,0 +1,23 @@
+import smtplib
+import os
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def send_email(konu, icerik):
+    gmail_user = os.getenv("EMAIL_USER")
+    gmail_password = os.getenv("EMAIL_PASS")
+
+    msg = MIMEText(icerik)
+    msg["Subject"] = konu
+    msg["From"] = gmail_user
+    msg["To"] = gmail_user
+
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+            server.login(gmail_user, gmail_password)
+            server.sendmail(gmail_user, [gmail_user], msg.as_string())
+            print("[MAIL] E-posta gönderildi.")
+    except Exception as e:
+        print(f"[MAIL HATA] Gönderilemedi: {e}")
